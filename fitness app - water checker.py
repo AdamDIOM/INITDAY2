e550ab1a2c94d99a592ac2463b30f1ea7618c732
Adam Cameron Drummond <66436400+AdamDIOM@users.noreply.github.com>
2021-06-28 23:53:01 +0100

    Add files via upload

---
+++ fitness app - water checker.py
@@ -0,0 +1,6 @@
+print("how much water have you drunk today? Enter a number in litres.")
+
+if int(input()) < 3:
+    print("you have not drunk enough water; please drink more!")
+else:
+    print("congratulations! you are drinking a healthy amount of water!")
